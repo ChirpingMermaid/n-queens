28b2093a002ab1722cf5db554ffb01cd87930923
chirpingmermaid <bluejello@gmail.com>
2016-08-03 21:31:10 -0700

    Implement findNRooksSolution and countNRooksSolutions

--- src/solvers.js
+++ src/solvers.js
@@ -29,6 +29,8 @@ window.findNRooksSolution = function(n) {
   // };
   //n = 4;
   var rooks = 0;
+  //var boardObject = new Board({n: n});
+  //var board = boardObject.rows();
   var board = new Board({n: n});
   // var index = 0;
   // for (var i = 0; i < n; i++) {
@@ -36,9 +38,6 @@ window.findNRooksSolution = function(n) {
   //   row[index] = 1;
   //   index++;
   // }
-
-  // console.log(board);
-  // console.log(board.rows());
 
   //var board = createBoard(n);
 
@@ -59,7 +58,7 @@ window.findNRooksSolution = function(n) {
   //console.log("hello", board);
   // //console.log("get", board.get(0));
 
-  var solution = board.rows(); //fixme
+  var solution = board.rows(); 
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
@@ -70,29 +69,79 @@ window.countNRooksSolutions = function(n) {
 
   // var rooks = 0;
   // var board = new Board({n: n});
-  var solutionCount = 0;
+  // var factorial = function(number) {
+  //   var num = number;
+  //   var answer = 1;
+  //   while (num > 1) {
+  //     answer *= num;
+  //     num--;
+  //   }
+  //   return answer;
+  // };
 
-  var combinations = function(n) {
-    
+  //n = 2;
+  var solutionCount = 0;
+  var boardObject = new Board({n: n});
+  var firstRowCount = n;
+  var container = [];
+  var countSolutions = function(boardObject, rooks) { 
+    var board = boardObject.rows();
+    var boardCopy = [];
+    for (var i = 0; i < board.length; i++) {
+      boardCopy.push(board[i].slice());
+    }
+    //var changed = true;
+    if (rooks === n) {
+      solutionCount++;
+      container.push(boardCopy);
+      return; 
+    }
+    for (var i = 0; i < n; i++) {
+      for (var j = 0; j < n; j++) {
+        if (board[i][j] === 0) {
+          board[i][j] = 1;
+          rooks++;
+          if (boardObject.hasAnyRooksConflicts()) {
+            board[i][j] = 0;
+            rooks--;
+            //changed = false;
+          } else {
+            countSolutions(boardObject, rooks);
+            board[i][j] = 0;
+            rooks--;
+          }
+        }
+      }
+    }
   };
 
+  countSolutions(boardObject, 0);
 
-  // for (var i = 0; i < n; i++) {
-  //   var row = board.get(i);
-  //   for (var j = 0; j < n; j++) {
-  //     row[j] = 1;
-  //     rooks++;
-  //     if (board.hasAnyRooksConflicts()) {
-  //       row[j] = 0;
-  //       rooks--;
-  //     }
-  //     if (rooks === n) {
-  //       solutionCount++;
-  //       break;
-  //     }
-  //   }
-  // }
-  var solution = undefined; //fixme
+  var uniq = function(array) {
+    var result = [];
+
+    for (var i = 0; i < array.length; i++) {
+      var elem = JSON.stringify(array[i]);
+      if (result.indexOf(elem) === -1) {
+        result.push(elem);
+      }
+    }
+    return result;
+  };
+
+  var newContainer = uniq(container);
+
+  solutionCount = newContainer.length;
+
+
+
+  //console.log('testing', container);
+  //var solution = solutionCount; 
+
+
+  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
+  return solutionCount;
+  //var solutionCount = factorial(n); //fixme
 
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
@@ -101,7 +150,25 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  var queens = 0;
+  var board = new Board({n: n});
+
+  for (var i = 0; i < n; i++) {
+    var row = board.get(i);
+    for (var j = 0; j < n; j++) {
+      row[j] = 1;
+      rooks++;
+      if (board.hasAnyRooksConflicts()) {
+        row[j] = 0;
+        rooks--;
+      }
+      if (rooks === n) {
+        break;
+      }
+    }
+  }
+
+  var solution = board.rows(); 
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
